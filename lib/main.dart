6d60e778e3a69652de8be425406a8d43b159780b
Naseem Srour <naseemsr94@gmail.com>
2021-02-19 19:39:28 +0200

    Parse json Example

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'parse_json.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyListApp());
 
 class MyApp extends StatelessWidget {
   @override
